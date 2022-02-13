e1ff75db2aa8eacbaaead2c469dec134074016e2
Bora Fatih KAZANCI <fatih.kazanci@ug.bilkent.edu.tr>
2022-02-13 16:56:46 +0300

    Pistol moves according to mouse direction, but now bullets moves according to mouse's direction simultaneously

--- Assets/Scripts/GunScripts/Gun.cs
+++ Assets/Scripts/GunScripts/Gun.cs
@@ -5,6 +5,11 @@ namespace GunScripts
 {
     public class Gun : MonoBehaviour
     {
+        //gun movement
+        public Rigidbody2D rb_g;
+        public SpriteRenderer sr_g;
+        private Vector3 gunDirection;
+
         public float gunHealth;
         public float gunDamage;
         public float gunFireRate;
@@ -15,8 +20,7 @@ namespace GunScripts
 
         private bool fired = false;
         private float firedTimer = 0f;
-    
-    
+
         // Start is called before the first frame update
         void Start()
         {
@@ -51,6 +55,34 @@ namespace GunScripts
                 GunState.StateTrigger(gunInformation, mouseClickInformation); //Aka Shoot most probably in our demo
 
             }
+
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 5.23f;
+
+            gunDirection = Camera.main.WorldToScreenPoint(transform.position);
+            mousePos.x = mousePos.x - gunDirection.x;
+            mousePos.y = mousePos.y - gunDirection.y;
+
+            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+            Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+            if (worldPosition.x < transform.position.x) 
+            {
+                sr_g.flipX = true;
+                mousePos = Input.mousePosition;
+                mousePos.z = 5.23f;
+
+                gunDirection = Camera.main.WorldToScreenPoint(transform.position);
+                mousePos.x = gunDirection.x - mousePos.x;
+                mousePos.y = gunDirection.y - mousePos.y;
+
+                angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            }
+            else if (worldPosition.x > transform.position.x) sr_g.flipX = false;
         }
     }
 }
