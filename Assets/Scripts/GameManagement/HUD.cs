d3df293a738907b1d8a04c1c5e651023b47449c9
boracun <cun2424@gmail.com>
2022-02-13 16:46:02 +0300

    Set HUD component references

---
+++ Assets/Scripts/GameManagement/HUD.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    //Right side
+    public Text healthText;
+    public Text evolutionMeterText;
+    public Text pandaSpeechText;
+
+    //Left side
+    public Text firepowerText;
+    public Text fireRateText;
+    public Button firepowerButton;
+    public Button fireRateButton;
+    public Image currentGun;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
